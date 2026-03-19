224bfad47f45e596ba604b16254b51a611754be1
Susan Shrestha <shresthasusan44@gmail.com>
2026-03-19 22:19:02 +0545

    feat: add new animated full-screen navigation component with branding, links, and contact information.

--- components/ui/InfoSection.tsx
+++ components/ui/InfoSection.tsx
@@ -101,7 +101,7 @@ export default function InfoSection({
           </p>
         </div>
         {/* Opening Hours */}
-        <div className=" max-w-sm md:max-w-full ">
+        <div className="max-w-xs md:max-w-full ">
           <p className={`caption uppercase text-${themeColor} flex items-center gap-2 mb-6`}>
             <span className={`w-2 h-2 bg-${themeColor}  inline-block shrink-0`} />
             {openingHoursLabel}
